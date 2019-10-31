00dd63e660f1c20ce44c4f4a7d5d01dbb957270b
FuHuaHuanYing <814222914@qq.com>
2019-10-31 14:15:46 +0800

    20191031adduser功能

--- public/assets/js/user.js
+++ public/assets/js/user.js
@@ -21,7 +21,7 @@ $('#userForm').on('submit',function(){
 
 //添加用户头像和预览功能
 //1.给input绑定change事件
-$('#avatar').on('change', function () {
+$('#modifyBox').on('change','#avatar',function () {
 	// 用户选择到的文件
 	// this.files[0]
 	var formData = new FormData();
@@ -43,3 +43,83 @@ $('#avatar').on('change', function () {
 		}
 	})
 });
+
+//获取用户列表
+$.ajax({
+	url:'/users',
+	type:'get',
+	success:function(response){
+		// console.log(response);
+		// 使用模板引擎将数据和HTML字符串进行拼接
+		var html = template('userTpl',{data:response});
+		// console.log(html);
+		// 将拼接好的字符串显示在页面中
+		$('#userBody').html(html)
+
+	}
+})
+
+// 通过事件委托的方式为编辑按钮添加点击事件 
+$('#userBody').on('click','.edit',function(){
+	// 获取被点击用户的id值
+	var id = $(this).attr('data-id');
+	// 根据id获取用户的详细信息
+	// console.log(id);
+	$.ajax({
+		type:'get',
+		url:`/users/${id}`,
+		success:function(data){
+			console.log(data);
+			var html = template('modifyTpl', data);
+			$('#modifyBox').html(html);
+		}
+	})
+	
+// 编辑用户 为修改表单添加表单提交事件
+$('#modifyBox').on('submit',"#modifyForm",function(){
+	var formData = $(this).serialize();
+	// 获取要修改的那个用户的id值
+	var id = $(this).attr('data-id');
+    // 向服务器端发送修改用户的请求
+	$.ajax({
+		type: 'put',
+		url:`/users/${id}` ,
+		data: formData,
+		success: function () {
+			// 刷新页面
+			location.reload();
+		}
+	})
+    // console.log(formData);
+    //// 阻止表单的默认提交行为
+    return false;	
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
